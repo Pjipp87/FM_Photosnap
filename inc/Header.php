fed488140ccfa804060152b48836575e9cc88b1d
ScarfaceHBC <p.jipp87@gamil.com>
2022-10-07 11:37:00 +0200

    ...

---
+++ inc/Header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
+    <link rel="stylesheet" href="../style/navigation.css">
+    <link rel="stylesheet" href="../src/font/DM_sans.css">
+
+    <script src="../src/jQuery/jquery-3.6.1.js"></script>
+    <script src="../src/bootstrap-5.2.2/dist/js/bootstrap.js"></script>
+    <title>Frontend Mentor | Photosnap Website Challenge</title>
+</head>
+
+<body>
+    <?php require_once "Navigation.php" ?>
+    <main>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+$pageTitle = "Home";
+$page = "home";
+require_once "./inc/Header.php" ?>
+
+
+
+<?php require_once "./inc/footer.php" ?>
