3b93eb124fae14c3020a0c6a493634c435ef096f
ScarfaceHBC <p.jipp87@gamil.com>
2022-10-07 14:21:43 +0200

    .....

--- index.php
+++ index.php
@@ -4,18 +4,49 @@ $page = "home";
 require_once "./inc/Header.php" ?>
 
 <div class="container-fluid p-0">
-    <img class="img-fluid" src="./assets/home/desktop/create-and-share.jpg"></img>
+    <img class="img-fluid" src="./assets/home/mobile/create-and-share.jpg"></img>
 
-    <div class="bg-dark text-white container-fluid text-container">
+    <div class="bg-black text-white container-fluid text-container_first">
         <div class="accent"></div>
         <h1 class="heading-home">Create and share your photo Stories.</h1>
-        <p class="text-home">
+        <p class="text-home_first">
             Photosnap is a platform for photographers and visual storytellers. We make it
             easy to share photos, tell stories and connect with others.
         </p>
-        <div class="btn btn-primary">Temp</div>
+        <div class="text-white">Temp</div>
 
     </div>
+</div>
+
+<div class="container-fluid p-0">
+    <img class="img-fluid" src="./assets/home/mobile/beautiful-stories.jpg"></img>
+
+    <div class="bg-white text-black container-fluid text-container">
+
+        <h1 class="heading-home">Beautiful stories<br>every time</h1>
+        <p class="text-home">
+            We provide design templates to ensure your stories look terrific. Easily add
+            photos, text, embed maps and media from other networks. Then share your story
+            with everyone.
+        </p>
+        <div class="text-black">Temp</div>
+
+    </div>
+</div>
+<div class="container-fluid p-0">
+    <img class="img-fluid" src="./assets/home/mobile/designed-for-everyone.jpg"></img>
+
+    <div class="bg-white text-black container-fluid text-container">
+
+        <h1 class="heading-home">Designed for everyone</h1>
+        <p class="text-home">
+            Photosnap can help you create stories that resonate with your audience. Our
+            tool is designed for photographers of all levels, brands, businesses you name it.
+        </p>
+        <a class="arrow_button">VIEW THE STORIES</a>
+
+    </div>
+</div>
 
 
-    <?php require_once "./inc/footer.php" ?>
+<?php require_once "./inc/footer.php" ?>
