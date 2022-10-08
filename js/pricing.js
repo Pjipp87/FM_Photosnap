5990141eaa914383f02c470a238a959b3e4c62ca
ScarfaceHBC <p.jipp87@gamil.com>
2022-10-08 19:06:04 +0200

    ...

--- js/pricing.js
+++ js/pricing.js
@@ -1,6 +1,26 @@
 $(document).ready(() => {
+  var state = 0;
+  var priceBasicArr = ["$19.00", "$190.00"];
+  var priceProArr = ["$39.00", "$390.00"];
+  var priceBusinessArr = ["$99.00", "$990.00"];
+  var timeArr = ["per month", "per year"];
+
+  $("#priceBasic").text(priceBasicArr[state]);
+  $("#pricePro").text(priceProArr[state]);
+  $("#priceBusiness").text(priceBusinessArr[state]);
+  $(".time").text(timeArr[state]);
+
   $("#flexSwitchCheckDefault").change(() => {
+    if ($("#flexSwitchCheckDefault").is(":checked")) {
+      state++;
+    } else {
+      state--;
+    }
     $("#monthly").toggleClass(" active");
     $("#yearly").toggleClass(" active");
+    $("#priceBasic").text(priceBasicArr[state]);
+    $("#pricePro").text(priceProArr[state]);
+    $("#priceBusiness").text(priceBusinessArr[state]);
+    $(".time").text(timeArr[state]);
   });
 });
--- pages/pricing.php
+++ pages/pricing.php
@@ -13,14 +13,43 @@ require_once "../inc/Header.php" ?>
     </div>
 </div>
 
-<div class="ContainerPricingSwitch">
+<div class="containerOffer">
+    <div class="ContainerPricingSwitch">
 
-    <div class="form-check form-switch d-flex ">
-        <label class="form-check-label active" for="flexSwitchCheckDefault" id="monthly">Monthly</label>
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-        <label class="form-check-label" for="flexSwitchCheckDefault" id="yearly">Yearly</label>
+        <div class="form-check form-switch d-flex ">
+            <label class="form-check-label active" for="flexSwitchCheckDefault" id="monthly">Monthly</label>
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+            <label class="form-check-label" for="flexSwitchCheckDefault" id="yearly">Yearly</label>
+        </div>
+
     </div>
 
+    <div class="containerPricing">
+        <h1>Basic</h1>
+        <p>Includes basic usage of our platform. Recommended for new and aspiring photographers.</p>
+        <h2 id="priceBasic"></h2>
+        <h5 class="time"></h5>
+        <a href="#">Pick Plan</a>
+    </div>
+    <div class="containerPricing">
+        <div class="accent"></div>
+        <h1>Basic</h1>
+        <p>More advanced features available. Recommended for photography veterans and professionals.</p>
+        <h2 id="pricePro"></h2>
+        <h5 class="time"></h5>
+        <a href="#">Pick Plan</a>
+    </div>
+    <div class="containerPricing">
+        <h1>Basic</h1>
+        <p>Additional features available such as more detailed metrics. Recommended for business owners.</p>
+        <h2 id="priceBusiness"></h2>
+        <h5 class="time"></h5>
+        <a href="#">Pick Plan</a>
+    </div>
 </div>
 
+<h1>Tabelle</h1>
+
+
+<?php require_once "../inc/beta.php"; ?>
 <?php require_once "../inc/footer.php" ?>
