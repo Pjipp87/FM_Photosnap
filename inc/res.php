5576b7f4a3f9000b2e60c3626289ec62fc5dd51a
ScarfaceHBC <p.jipp87@gamil.com>
2022-10-09 17:10:47 +0200

    ....

--- inc/res.php
+++ inc/res.php
@@ -12,7 +12,14 @@ document.location=\"$PHP_SELF?r=1&width=\"+screen.width+\"&Height=\"+screen.heig
 else {
 // Code to be displayed if resolutoin is detected
      if(isset($_GET['width']) && isset($_GET['Height'])) {
-        $res= $_GET['width'];
+        if($_GET['width']>= 1440){
+            $res= "desktop";
+        } else if($res= $_GET['width'] < 1440 && $res= $_GET['width'] >=768){
+            $res= "tablet";
+        } else if($res= $_GET['width'] < 768){
+            $res= "mobile";
+        }
+        
      }
      else {
                // Resolution not detected
